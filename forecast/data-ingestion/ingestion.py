f11f9d3c9041a2802aa80ef940da97c5babd6ab9
Krishnaji Rao H <krishnajirao.h@gmail.com>
2022-09-10 22:15:24 +0530

    Data ingestion

--- forecast/data-ingestion/ingestion.py
+++ forecast/data-ingestion/ingestion.py
@@ -1,15 +1,25 @@
 import coinmarketcapapi
+from github import Github,InputGitAuthor
 import pandas as pd
     
 
 def inject():
-    cmc = coinmarketcapapi.CoinMarketCapAPI('6d84d06d-0978-4881-8be1-43a88c3f5e26')
+    cmc_token = '6d84d06d-0978-4881-8be1-43a88c3f5e26'
+    github_access_token = "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"    
+
+    cmc = coinmarketcapapi.CoinMarketCapAPI(cmc_token)
+    g = Github(github_access_token)
 
     data_listing = cmc.cryptocurrency_listings_latest()
+    df = pd.DataFrame(data_listing.data)    
 
-    df = pd.DataFrame(data_listing.data)
-    print(df.head())
-
+    repo = g.get_repo("krishnajiraoh/my-crypto-world")    
+    path = "forecast/data/history_data.csv"
+    contents = repo.get_contents(path)
+    content = df.to_csv(index=False)
+    message = "updated using PyGithub API"
+    
+    repo.update_file(path, message, content, contents.sha , branch="main") 
 
 if __name__ == '__main__':
     inject()
