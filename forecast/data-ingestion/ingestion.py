bdaa4a5f1965a128151a682dba8a4de69065d76f
Krishnaji Rao H <krishnajirao.h@gmail.com>
2022-09-12 19:22:20 +0530

    Update ingestion.py

--- forecast/data-ingestion/ingestion.py
+++ forecast/data-ingestion/ingestion.py
@@ -4,19 +4,15 @@ import pandas as pd
 import os
     
 
-def inject():
+def upload_data(df):
     github_access_token = os.environ.get('GH_ACCESS_TOKEN') 
-    
-    cg = CoinGeckoAPI()
+    print(github_access_token)
     g = Github(github_access_token)
 
-    data = cg.get_coin_ohlc_by_id(id='bitcoin', vs_currency="usd", days="30")
-    cols= ["Time_in_ms", "Open", "High", "Low", "Close"]
-    df = pd.DataFrame(data, columns=cols)   
-    #df["Time"] = pd.to_datetime(df.Time, unit='ms')
+    repo_path = os.environ.get('GITHUB_REPOSITORY') 
+    repo = g.get_repo(repo_path)
 
-    repo = g.get_repo("krishnajiraoh/my-crypto-world")    
-    path = "forecast/data/history_data.csv"
+    path = os.environ.get('HISTORY_DATA_PATH') 
     contents = repo.get_contents(path)
     content = df.to_csv(index=False)
     message = "Updated by Ingestion script using PyGithub API"
@@ -24,5 +20,17 @@ def inject():
     repo.update_file(path, message, content, contents.sha , branch="main") 
     print("Data Injected successfully")
 
+def inject():
+    cg = CoinGeckoAPI()
+    
+    coin = os.environ.get('COIN') 
+    print(coin)
+    days = os.environ.get('LOOK_BACK_DAYS') 
+    data = cg.get_coin_ohlc_by_id(id=coin, vs_currency="usd", days=days)
+    cols= ["Time_in_ms", "Open", "High", "Low", "Close"]
+    df = pd.DataFrame(data, columns=cols)   
+    #df["Time"] = pd.to_datetime(df.Time, unit='ms')
+    upload_data(df)
+    
 if __name__ == '__main__':
     inject()
